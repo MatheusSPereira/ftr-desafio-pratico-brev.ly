f7cdd4d6f39712eba7ed73c7a235cff3ce24eb5d
MatheusSPereira <githubmatheus@outlook.com>
2026-07-07 23:02:41 -0300

    feat: implement not-found page

--- web/src/pages/not-found/index.tsx
+++ web/src/pages/not-found/index.tsx
@@ -1,3 +1,13 @@
+import { Link } from 'react-router-dom'
+
 export function NotFoundPage() {
-  return <main className="p-4">Not found</main>
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center gap-4 p-4 text-center">
+      <h1 className="text-2xl font-bold">404</h1>
+      <p>Link não encontrado</p>
+      <Link to="/" className="underline">
+        Voltar para a home
+      </Link>
+    </main>
+  )
 }
